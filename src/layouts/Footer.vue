9939d419bd88bad6a826fe14438c39839f10fc1a
XIVAdev <131533046+XIVAdev@users.noreply.github.com>
2023-10-28 12:57:52 +0500

    Update Footer.vue
    Footer tayyor

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,3 +1,58 @@
 <template>
-  <footer>Footer</footer>
+      <footer>
+      <div class=" bg-black container mx-auto">
+        <div class="flex justify-evenly text-white p-5 ">
+          <div class="">
+            <h2 class="text-xl my-3 ">Exclusive</h2>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Subscribe</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Get 10% off your first order</a><br>
+            <input type="text" class="bg-black border p-1 my-2" placeholder="Enter your email" />
+          </div>
+          <div class="">
+            <h2 class="text-xl my-3 ">Support</h2>
+            <a class="font-normal my-2 text-background: #FAFAFA;">111 Bijoy sarani, Dhaka,<br> DH 1515, Bangladesh.</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">exclusive@gmail.com</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">+88015-88888-9999</a><br>
+          </div>
+          <div class="">
+            <h2 class="text-xl my-3 ">Account</h2>
+            <a class="font-normal my-2 text-background: #FAFAFA; ">My Account</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Login / Register</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Cart</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Wishlist</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Shop</a><br>
+          </div>
+          <div class="">
+            <h2 class="text-xl my-3 ">Quick Link</h2>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Privacy Policy</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Terms Of Use</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">FAQ</a><br>
+            <a class="font-normal my-2 text-background: #FAFAFA;">Contact</a><br>
+          </div>
+          <div class="">
+            <h2 class="text-xl my-3 ">Download App</h2>
+            <p>Save $3 with App New User Only</p>
+            <div class="flex gap-3">
+              <div>
+                <a href=""><img src="/poblic/Qr Code.png" alt="Qrd"></a>
+              </div>
+              <div class="">
+                <a href=""><img src="/poblic/png-transparent-google-play-store-logo-google-play-app-store-android-wallets-text-label-logo.png" alt="play market"></a>
+                <a href=""><img src="/poblic/download-appstore.png" alt="app store" class="my-3"></a>
+              </div>
+            </div>
+            <div class="flex gap-5">
+              <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+              <a href="">   <i class="fa-brands fa-x-twitter"></i></a>
+              <a href="">       <i class="fa-brands fa-instagram"></i></a>
+              <a href=""><i class="fa-brands fa-telegram"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="p-3 border-solid border-t-2 border-zinc-500"> 
+          <h2 class="text-zinc-500 text-center ">Copyright Rimel 2022. All right reserved</h2>
+        </div>
+      </div>
+    
+    </footer>
 </template>
