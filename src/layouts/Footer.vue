7789f2f5472e6016b2cb94b6478e6370dac2cfa9
XIVAdev <131533046+XIVAdev@users.noreply.github.com>
2023-11-14 16:36:28 +0500

    salom
    salomlar

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,58 +1,65 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <template>
-      <footer>
-      <div class=" bg-black container mx-auto">
-        <div class="flex justify-evenly text-white p-5 ">
-          <div class="">
-            <h2 class="text-xl my-3 ">Exclusive</h2>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Subscribe</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Get 10% off your first order</a><br>
-            <input type="text" class="bg-black border p-1 my-2" placeholder="Enter your email" />
+  <footer class="bg-black text-white">
+    <div class="container mx-auto p-5  md:flex justify-between">
+      <div class="mb-4 md:mb-0">
+        <h2 class="text-xl md:text-lg font-bold mb-2">Exclusive</h2>
+        <a href="#" class="block mb-2 text-sm">Subscribe</a>
+        <a href="#" class="block mb-2 text-sm">Get 10% off your first order</a>
+        <input type="text" class="bg-black border p-1 w-full md:w-auto" placeholder="Enter your email" />
+      </div>
+
+   
+      <div class="mb-4 md:mb-0">
+        <h2 class="text-xl md:text-lg font-bold mb-2">Support</h2>
+        <p class="mb-2 text-sm">111 Bijoy sarani, Dhaka,<br> DH 1515, Bangladesh.</p>
+        <a href="mailto:exclusive@gmail.com" class="block mb-2 text-sm">exclusive@gmail.com</a>
+        <a href="tel:+88015888889999" class="block mb-2 text-sm">+88015-88888-9999</a>
+      </div>
+
+
+      <div class="mb-4 md:mb-0">
+        <h2 class="text-xl md:text-lg font-bold mb-2">Account</h2>
+        <a href="#" class="block mb-2 text-sm">My Account</a>
+        <a href="#" class="block mb-2 text-sm">Login / Register</a>
+        <a href="#" class="block mb-2 text-sm">Cart</a>
+        <a href="#" class="block mb-2 text-sm">Wishlist</a>
+        <a href="#" class="block mb-2 text-sm">Shop</a>
+      </div>
+
+
+      <div class="mb-4 md:mb-0">
+        <h2 class="text-xl md:text-lg font-bold mb-2">Quick Link</h2>
+        <a href="#" class="block mb-2 text-sm">Privacy Policy</a>
+        <a href="#" class="block mb-2 text-sm">Terms Of Use</a>
+        <a href="#" class="block mb-2 text-sm">FAQ</a>
+        <a href="#" class="block mb-2 text-sm">Contact</a>
+      </div>
+
+    
+      <div>
+        <h2 class="text-xl md:text-lg font-bold mb-2">Download App</h2>
+        <p class="mb-2 text-sm">Save $3 with App New User Only</p>
+        <div class="flex gap-3">
+          <div>
+            <a href="#"><img src="./img/Qr Code (1).png" class="mt-1" alt="Qr Code"></a>
           </div>
-          <div class="">
-            <h2 class="text-xl my-3 ">Support</h2>
-            <a class="font-normal my-2 text-background: #FAFAFA;">111 Bijoy sarani, Dhaka,<br> DH 1515, Bangladesh.</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">exclusive@gmail.com</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">+88015-88888-9999</a><br>
-          </div>
-          <div class="">
-            <h2 class="text-xl my-3 ">Account</h2>
-            <a class="font-normal my-2 text-background: #FAFAFA; ">My Account</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Login / Register</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Cart</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Wishlist</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Shop</a><br>
-          </div>
-          <div class="">
-            <h2 class="text-xl my-3 ">Quick Link</h2>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Privacy Policy</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Terms Of Use</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">FAQ</a><br>
-            <a class="font-normal my-2 text-background: #FAFAFA;">Contact</a><br>
-          </div>
-          <div class="">
-            <h2 class="text-xl my-3 ">Download App</h2>
-            <p>Save $3 with App New User Only</p>
-            <div class="flex gap-3">
-              <div>
-                <a href=""><img src="/poblic/Qr Code.png" alt="Qrd"></a>
-              </div>
-              <div class="">
-                <a href=""><img src="/poblic/png-transparent-google-play-store-logo-google-play-app-store-android-wallets-text-label-logo.png" alt="play market"></a>
-                <a href=""><img src="/poblic/download-appstore.png" alt="app store" class="my-3"></a>
-              </div>
-            </div>
-            <div class="flex gap-5">
-              <a href=""><i class="fa-brands fa-facebook-f"></i></a>
-              <a href="">   <i class="fa-brands fa-x-twitter"></i></a>
-              <a href="">       <i class="fa-brands fa-instagram"></i></a>
-              <a href=""><i class="fa-brands fa-telegram"></i></a>
-            </div>
+          <div>
+            <a href="#"><img src="./img/GooglePlay.png" alt="Play Market"></a>
+            <a href="#"><img src="./img/download-appstore (1).png" alt="App Store" class="my-3"></a>
           </div>
         </div>
-        <div class="p-3 border-solid border-t-2 border-zinc-500"> 
-          <h2 class="text-zinc-500 text-center ">Copyright Rimel 2022. All right reserved</h2>
+        <div class="flex gap-5">
+          <a href="#"><i class="fa-brands fa-facebook text-white"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-telegram"></i></a>
         </div>
       </div>
-    
-    </footer>
+    </div>
+
+    <div class="text-center p-3 border-t-2 border-gray-500">
+      <p class="text-gray-500 text-sm">Copyright Rimel 2022. All right reserved</p>
+    </div>
+  </footer>
 </template>
