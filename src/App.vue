560bf3b57f1ed628da46e277e7711e882203db4b
XIVAdev <131533046+XIVAdev@users.noreply.github.com>
2023-11-17 14:58:29 +0500

    salom
    salom

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import Header from "./layouts/Header.vue";
 import Footer from "./layouts/Footer.vue";
+import Arrival from "./sections/arrival.vue"
 export default {
   components: {
     Header,
     Footer,
+    Arrival
   },
 };
 </script>
@@ -14,5 +16,6 @@ export default {
   <div>
     <h2>App Vue</h2>
   </div>
+  <Arrival/>
   <Footer />
 </template>
---
+++ src/sections/arrival.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex gap-3 items-center">
+      <div class="bg-[#DB4444] w-5 h-8 rounded-md"></div>
+      <div class="text-[#DB4444] font-semibold items-center">
+        <p>Featured</p>
+      </div>
+    </div>
+    <div class="text-4xl m-0 font-semibold my-2">
+      <h2>New Arrival</h2>
+    </div>
+    <div class="flex justify-between gap-3 my-5 mt-8 rounded-sm">
+      <div class="w-1/2">
+        <img
+          class="w-full h-full object-cover"
+          src="../layouts/img/Frame 684.png"
+          alt=""
+        />
+      </div>
+
+      <div class="w-1/2 gap-3">
+        <div class="">
+          <img
+            class="w-full h-full object-cover"
+            src="../layouts/img/Frame 685.png"
+            alt=""
+          />
+        </div>
+        <div class="flex gap-3 mt-3">
+          <div class="w-1/2 h-1/2">
+            <img
+              class="w-full h-full object-cover"
+              src="../layouts/img/Frame 686.png"
+              alt=""
+            />
+          </div>
+          <div class="w-1/2 h-1/2">
+            <img
+              class="w-full h-full object-cover"
+              src="../layouts/img/Frame 687.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
