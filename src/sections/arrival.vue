f3af4ca458f66ae4b8295682cf660b4adc8b0975
XIVAdev <131533046+XIVAdev@users.noreply.github.com>
2023-11-19 11:55:19 +0500

    salom
    salom

--- src/sections/arrival.vue
+++ src/sections/arrival.vue
@@ -11,34 +11,34 @@
     </div>
     <div class="flex justify-between gap-x-3 h-[800px] my-5 mt-8 rounded-sm">
       <!-- Large Image -->
-      <div class="relative w-1/2 h-full bg-blue-500">
+      <div class="relative w-1/2 h-full bg-black">
         <img
           class="w-full h-full object-cover"
-          src="../layouts/img/Frame 684.png"
+          src="../layouts/img/ps5.png"
           alt=""
         />
-        <div class="absolute bottom-5 left-5 text-white w-[300px] bg-red-500">
-          <h2>PlayStation 5</h2>
-          <p>Black and White version of the PS5 coming out on sale.</p>
-          <a href="">Shop now</a>
+        <div class="absolute bottom-10 left-10 text-[#FAFAFA] w-[300px] ">
+          <h2 class="text-4xl">PlayStation 5</h2>
+          <p class="my-3">Black and White version of the PS5 coming out on sale.</p>
+          <a href="£" class="underline  underline-offset-4 text-2xl ">Shop now</a>
         </div>
       </div>
       <!-- Category Images -->
       <div class="w-1/2 h-full flex flex-col gap-y-3">
-        <img
-          class="w-full h-1/2 object-cover"
-          src="../layouts/img/Frame 685.png"
-          alt=""
-        />
+        <div class="">
+
+          
+        </div>
+        
         <div class="flex gap-3 flex-grow">
           <img
             class="w-full h-full object-cover"
-            src="../layouts/img/Frame 686.png"
+            src="../layouts/img/speaker.png"
             alt=""
           />
           <img
             class="w-full h-full object-cover"
-            src="../layouts/img/Frame 687.png"
+            src="../layouts/img/perfume.png"
             alt=""
           />
         </div>
