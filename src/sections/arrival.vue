398d2d62effc3e2d0c3f6697e473e0f573f42a59
XIVAdev <131533046+XIVAdev@users.noreply.github.com>
2023-11-22 16:03:13 +0500

    salom
    salom

--- src/sections/arrival.vue
+++ src/sections/arrival.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto">
     <div class="flex gap-3 items-center">
       <div class="bg-[#DB4444] w-5 h-8 rounded-md"></div>
-      <div class="text-[#DB4444] font-semibold items-center">
+      <div class="text-[#DB4444] font-inter items-center">
         <p>Featured</p>
       </div>
     </div>
@@ -11,7 +11,7 @@
     </div>
     <div class="flex justify-between gap-x-3 h-[800px] my-5 mt-8 rounded-sm">
       <!-- Large Image -->
-      <div class="relative w-1/2 h-full bg-black">
+      <div class="relative w-1/2 rounded-md h-full bg-black">
         <img
           class="w-full h-full object-cover"
           src="../layouts/img/ps5.png"
@@ -27,26 +27,81 @@
       </div>
       <!-- Category Images -->
       <div class="w-1/2 h-full flex flex-col gap-y-3">
-        <div class="gap-3 flex-grow flex  bg-[#0D0D0D] h-1/2 ">
-          <div class="w-1/2"></div>
-          <img class="w-1/2 h-full object-cover" src="../layouts/img/women.png" alt="women">
+        <div
+          class="gap-3 flex-grow flex bg-[#0D0D0D] h-1/2 rounded-md relative"
+        >
+          <div class="w-1/2">
+            <div class="absolute bottom-10 left-10 text-[#FAFAFA] w-[300px]">
+              <h2 class="text-4xl">Women’s Collections</h2>
+              <p class="my-3">
+                Featured woman collections that give you another vibe.
+              </p>
+              <a href="£" class="underline underline-offset-4 text-2xl"
+                >Shop now</a
+              >
+            </div>
+          </div>
+          <img
+            class="w-1/2 h-full object-cover rounded-md"
+            src="../layouts/img/women.png"
+            alt="women"
+          />
         </div>
 
-        <div class="flex gap-3 flex-grow h-1/2">
-          <div class="bg-black w-1/2 grid place-items-center">
+        <div class="flex gap-3 flex-grow h-1/2 relative">
+          <div class="bg-black w-1/2 grid rounded-md place-items-center">
             <img
-              class="h-[80%] w-[80%] "
+              class="h-[80%] w-[80%]"
               src="../layouts/img/speaker.png"
               alt="speaker"
             />
+            <div class="absolute bottom-10 left-10 text-[#FAFAFA] w-[300px]">
+              <h2 class="text-3xl">Speakers</h2>
+              <p class="my-3">
+                Featured woman collections that give you another vibe.
+              </p>
+              <a href="£" class="underline underline-offset-4 text-xl"
+                >Shop now</a
+              >
+            </div>
           </div>
-          <div class="bg-black w-1/2"  >
+          <div
+            class="bg-black rounded-md w-1/2 grid place-items-center relative"
+          >
             <img
-              class="h-full w-full"
+              class="h-[80%] w-[80%]"
               src="../layouts/img/perfume.png"
               alt="po"
             />
+            <div class="absolute bottom-10 left-10 text-[#FAFAFA] w-[300px]">
+              <h2 class="text-3xl">Perfume</h2>
+              <p class="my-3">
+                GUCCI INTENSE OUD EDP
+              </p>
+              <a href="£" class="underline underline-offset-4 text-xl"
+                >Shop now</a
+              >
+            </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-evenly my-11">
+      <div class="w-20 h-20 bg-red-500 rounded-[50%] grid place-items-center">
+        <div class="w-14 h-14 bg-black rounded-[50%]">
+          <img src="" alt="">
+        </div>
+        <h2>FREE AND FAST DELIVERY</h2>
+        <p>Free delivery for all orders over $140</p>
+      </div>
+      <div class="">
+        <div class="">
+          <img src="" alt="">
+        </div>
+      </div>
+      <div class="">
+        <div class="">
+          <img src="" alt="">
         </div>
       </div>
     </div>
