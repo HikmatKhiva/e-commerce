4f98d0902a21a07ed104e1337cf35f2f3456bb00
HikmatKhiva <xikmatkhiva.4s@gmail.com>
2023-11-27 15:19:42 +0500

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
-<script setup>
-import Layout from "./layouts/Layout.vue";
-import FlashSection from "./sections/FlashSection.vue";
-import Category from "./sections/Category.vue";
-</script>
 <template>
-  <Layout>
-    <FlashSection />
-    <hr class="container mx-auto" />
-    <Category />
-  </Layout>
+  <component :is="$route?.meta.layout || 'div'">
+    <router-view />
+  </component>
 </template>
+<script setup></script>
