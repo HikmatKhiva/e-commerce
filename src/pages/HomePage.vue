8cccf6132eb7497956ebf62c33968d242f1eaffa
HikmatKhiva <xikmatkhiva.4s@gmail.com>
2023-11-28 15:46:09 +0500

    Update HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,8 +2,12 @@
   <FlashSection />
   <hr class="container mx-auto" />
   <Category />
+  <Arrival />
+  <ServiceSection />
 </template>
 <script setup>
 import FlashSection from "../sections/FlashSection.vue";
 import Category from "../sections/Category.vue";
+import ServiceSection from "../sections/ServiceSection.vue";
+import Arrival from "../sections/Arrival.vue";
 </script>
