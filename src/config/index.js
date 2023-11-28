9ae8e8c3ff1fdc06491de5dde681846fbc2afd8c
HikmatKhiva <xikmatkhiva.4s@gmail.com>
2023-11-28 15:45:52 +0500

    create new Section

--- src/config/index.js
+++ src/config/index.js
@@ -1,40 +1,60 @@
 import { phoneSvg } from "../assets";
 
-
 export const categories = [
-    {
-        id: 1,
-        img: phoneSvg,
-        title: "Phones"
-    },
-    {
-        id: 2,
-        img: phoneSvg,
-        title: "Phones"
-    },
-    {
-        id: 3,
-        img: phoneSvg,
-        title: "Phones"
-    },
-    {
-        id: 4,
-        img: phoneSvg,
-        title: "Phones"
-    },
-    {
-        id: 5,
-        img: phoneSvg,
-        title: "Phones"
-    },
-    {
-        id: 6,
-        img: phoneSvg,
-        title: "Phones"
-    },
-    {
-        id: 7,
-        img: phoneSvg,
-        title: "Phones"
-    },
-] 
+  {
+    id: 1,
+    img: phoneSvg,
+    title: "Phones",
+  },
+  {
+    id: 2,
+    img: phoneSvg,
+    title: "Phones",
+  },
+  {
+    id: 3,
+    img: phoneSvg,
+    title: "Phones",
+  },
+  {
+    id: 4,
+    img: phoneSvg,
+    title: "Phones",
+  },
+  {
+    id: 5,
+    img: phoneSvg,
+    title: "Phones",
+  },
+  {
+    id: 6,
+    img: phoneSvg,
+    title: "Phones",
+  },
+  {
+    id: 7,
+    img: phoneSvg,
+    title: "Phones",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon:"carbon:delivery",
+    title: "FREE AND FAST DELIVERY",
+    description: "Free delivery for all orders over $140",
+  },
+  {
+    id: 2,
+    icon:"mingcute:service-line",
+    title: "24/7 CUSTOMER SERVICE",
+    description: "Friendly 24/7 customer support",
+  },
+  {
+    id: 3,
+    icon:"ant-design:safety-outlined",
+    title: "MONEY BACK GUARANTEE",
+    description: "We reurn money within 30 days",
+  },
+];
---
+++ src/sections/ServiceSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="py-28">
+    <div class="flex justify-evenly">
+      <div
+        class="text-center font-poppins"
+        v-for="service in services"
+        :key="service.id"
+      >
+        <div
+          class="w-20 h-20 bg-[#c1c0c1] rounded-[50%] flex justify-center items-center mx-auto"
+        >
+          <div class="w-14 h-14 bg-black grid place-items-center rounded-[50%]">
+            <Icon :icon="service.icon" class="text-white" width="30" />
+          </div>
+        </div>
+        <h2 class="text-lg mt-5 font-medium">{{ service.title }}</h2>
+        <p class="text-sm">{{ service.description }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+import { Icon } from "@iconify/vue";
+import { services } from "../config";
+</script>
