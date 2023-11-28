d00a4cf90a54e0160be15d95ece65148eeacad7a
HikmatKhiva <xikmatkhiva.4s@gmail.com>
2023-11-28 15:50:41 +0500

    fixed bug

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,4 +1,4 @@
-export { default as Arrival } from "./Arrival.vue";
+export { default as Arrival } from "./ArrivalSection.vue";
 export { default as Category } from "./Category.vue";
 export { default as FlashSection } from "./FlashSection.vue";
 export { default as Hero } from "./Hero.vue";
