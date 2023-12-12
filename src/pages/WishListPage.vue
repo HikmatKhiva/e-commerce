9bd5cddc3ec4ec557fd3ff5900643ce957abd4f0
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2023-12-12 17:28:30 +0500

    Create WishListPage.vue

---
+++ src/pages/WishListPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="py-20">
+    <div class="container mx-auto">
+      <div class="flex justify-between">
+        <h2 class="text-xl font-poppins">Wishlist ({{ 4 }})</h2>
+        <router-link
+          to="/"
+          class="font-poppins font-medium border py-4 px-12 border-black inline-block rounded"
+          >Move All To Bag</router-link
+        >
+      </div>
+      <div class="flex justify-between mt-[60px]">
+        <WishCard v-for="item in 4" :key="item" />
+      </div>
+    </div>
+  </section>
+  <RecommendedSection />
+</template>
+
+<script setup>
+import RecommendedSection from "../sections/RecommendedSection.vue";
+import WishCard from "../components/WishCard.vue";
+</script>
