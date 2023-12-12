e94ff1120bf729fd76f5c443ab65006d9e92ca19
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2023-12-12 17:30:24 +0500

    Create RecommendedSection.vue

---
+++ src/sections/RecommendedSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="py-20">
+    <div class="container mx-auto">
+      <div class="flex justify-between items-start">
+        <SectionTopTitle title="Just For You" />
+        <router-link
+          to="/"
+          class="font-poppins font-medium border py-4 px-12 border-black inline-block rounded"
+          >See All</router-link
+        >
+      </div>
+      <div class="flex justify-between mt-[60px]">
+        <RecommendedCard v-for="item in 4" :key="item" />
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+import SectionTopTitle from "../components/SectionTopTitle.vue";
+import RecommendedCard from "../components/RecommendedCard.vue";
+</script>
