1347d768eb406cf760ae8024da46471ce2cf5db8
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2023-12-12 17:29:59 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import "./assets/css/style.css";
 import { router } from "./routes";
+import { plugin, defaultConfig } from "@formkit/vue";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import App from "./App.vue";
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(plugin, defaultConfig).mount("#app");
