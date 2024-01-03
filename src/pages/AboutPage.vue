aff374aa863aca5e12ec8e6fc9961c904317e714
XIVAdev <131533046+XIVAdev@users.noreply.github.com>
2024-01-03 19:41:58 +0500

    salom
    salom

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,3 +1,36 @@
 <template>
-    AboutPage
+  <section>
+    <div class="container mx-auto">
+      <div class="flex justify-start gap-x-2 py-10">
+        <router-link class="text-[#808080]" to="/">Home</router-link>
+        <span class="text-[#808080]">/</span>
+        <p>Contact</p>
+      </div>
+      <div class="flex justify-between">
+        <div class="w-1/2 self-center font-poppins text-[16px]">
+            <h2 class="text-[54px] font-inter font-semibold">
+                Our Story
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas facere, illum aliquid inventore voluptatibus saepe tenetur rem eum! Debitis, perspiciatis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque minus aperiam doloremque deleniti a, officia debitis voluptatem fugit? Natus, dicta!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius officia corrupti velit officiis molestiae consequatur. Vero modi ullam inventore sunt quo incidunt expedita.
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate dolor ipsa excepturi est?</p>
+        </div>
+        <div class="">
+            <img src="../assets/img/portrait-two-african-females-holding-shopping-bags-while-reacting-something-their-smartphone 1.png" alt="women">
+        </div>
+      </div>
+      <div class="flex justify-between">
+        <div class="">
+            <div class="">
+                <img src="../assets/img/icon_shop.p" alt="">
+            </div>
+            <h2>10.5k</h2>
+            <p>Sallers active our site</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
