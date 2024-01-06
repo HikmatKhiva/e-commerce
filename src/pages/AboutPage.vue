0198cc891733dd03600e6406e3a6cba05d0e6b9a
XIVAdev <131533046+XIVAdev@users.noreply.github.com>
2024-01-06 10:16:33 +0500

    about
    saLOM

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -8,29 +8,100 @@
       </div>
       <div class="flex justify-between">
         <div class="w-1/2 self-center font-poppins text-[16px]">
-            <h2 class="text-[54px] font-inter font-semibold">
-                Our Story
-            </h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas facere, illum aliquid inventore voluptatibus saepe tenetur rem eum! Debitis, perspiciatis!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque minus aperiam doloremque deleniti a, officia debitis voluptatem fugit? Natus, dicta!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius officia corrupti velit officiis molestiae consequatur. Vero modi ullam inventore sunt quo incidunt expedita.
-            </p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate dolor ipsa excepturi est?</p>
+          <h2 class="text-[54px] font-inter font-semibold">Our Story</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            facere, illum aliquid inventore voluptatibus saepe tenetur rem eum!
+            Debitis, perspiciatis! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Cumque minus aperiam doloremque deleniti a,
+            officia debitis voluptatem fugit? Natus, dicta! Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Eius officia corrupti velit
+            officiis molestiae consequatur. Vero modi ullam inventore sunt quo
+            incidunt expedita.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+            dolor ipsa excepturi est?
+          </p>
         </div>
         <div class="">
-            <img src="../assets/img/portrait-two-african-females-holding-shopping-bags-while-reacting-something-their-smartphone 1.png" alt="women">
+          <img
+            src="../assets/img/portrait-two-african-females-holding-shopping-bags-while-reacting-something-their-smartphone 1.png"
+            alt="women"
+          />
         </div>
       </div>
-      <div class="flex justify-between">
-        <div class="">
-            <div class="">
-                <img src="../assets/img/icon_shop.p" alt="">
+
+
+
+
+
+      <div class="justify-center items-center gap-[30px] flex">
+        <div
+          class="w-[270px] h-[230px] relative rounded border border-black border-opacity-30 hover:bg-[#DB4444] transition-all duration-300 text-white"
+        >
+          <div
+            class="left-[50px] top-[30px] absolute flex-col justify-start items-center gap-6 inline-flex"
+          >
+            <div class="w-20 h-20 relative">
+              <div class="w-20 h-20 left-0 top-0 absolute">
+                <div
+                  class="w-20 h-20 left-0 top-0 absolute opacity-30 bg-zinc-800 rounded-full"
+                ></div>
+                <div
+                  class="w-[58px] h-[58px] left-[11px] top-[11px] absolute bg-black rounded-full flex justify-center items-center"
+                >
+                  <img class="" src="../assets/img/icon_shop.png" alt="" />
+                </div>
+              </div>
+              <div class="w-10 h-10 left-[20px] top-[20px] absolute">
+                <div
+                  class="w-[32.66px] h-[30px] left-[3.66px] top-[5px] absolute"
+                ></div>
+              </div>
             </div>
-            <h2>10.5k</h2>
-            <p>Sallers active our site</p>
+            <div class="flex-col justify-start items-center gap-3 flex">
+              <h2
+                class="text-black text-[32px] font-bold font-['Inter'] leading-[30px] tracking-wider"
+              >
+                10.5k
+              </h2>
+              <p
+                class="text-center text-black text-base font-normal font-['Poppins'] leading-normal"
+              >
+                Sallers active our site
+              </p>
+            </div>
+          </div>
         </div>
       </div>
+
+
+      
+      <div class="w-[370px] h-[564px] flex-col justify-start items-start gap-8 inline-flex">
+  <div class="w-[370px] h-[430px] relative bg-neutral-100 rounded">
+    <img class="absolute" src="../assets/img/" />
+  </div>
+  <div class="flex-col justify-start items-start gap-4 flex">
+    <div class="flex-col justify-start items-start gap-2 flex">
+      <div class="text-black text-[32px] font-medium font-['Inter'] leading-[30px] tracking-wider">Tom Cruise</div>
+      <div class="text-black text-base font-normal font-['Poppins'] leading-normal">Founder & Chairman</div>
+    </div>
+    <div class="justify-start items-start gap-4 inline-flex">
+      <div class="w-6 h-6 relative">
+        <div class="w-[27.06px] h-6 left-[-5px] top-0 absolute">
+        </div>
+      </div>
+      <div class="w-6 h-6 relative">
+        <div class="w-[18px] h-[18px] left-[3px] top-[3px] absolute">
+        </div>
+      </div>
+      <div class="w-6 h-6 relative"></div>
+    </div>
+  </div>
+</div>
+
+
     </div>
   </section>
 </template>
